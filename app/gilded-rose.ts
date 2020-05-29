f524b33ff6f8b0f448df208d930eb5b4023b012a
Phil <p.vancaloen@hotmail.com>
2020-05-29 18:34:45 +0200

    increase & decrease quality methods

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -8,8 +8,14 @@ export class Item {
         this.sellIn = sellIn;
         this.quality = quality;
     }
-
+    public increaseQuality() {
+        return this.quality = this.quality + 1
+    }
+    public decreaseQuality() {
+        return this.quality = this.quality - 1
+    }
 }
+
 
 export class GildedRose {
     items: Array<Item>;
@@ -24,29 +30,29 @@ export class GildedRose {
                 // diminue la qualité de -1 
                 if (this.items[i].quality > 0) {
                     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
+                        this.items[i].decreaseQuality()
                     }
                 }
                 if (this.items[i].name == 'Conjured') {
                     if (this.items[i].quality > 0) {
-                        this.items[i].quality = this.items[i].quality - 1
+                        this.items[i].decreaseQuality()
                     }
                 }
             } else {
                 // j'augemente la quality de 1
                 if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
+                    this.items[i].increaseQuality()
                     // je continue à augmenté pour backstage
                     if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
                         // augemente backstage quality
                         if (this.items[i].sellIn < 11) {
                             if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
+                                this.items[i].increaseQuality()
                             }
                         }
                         if (this.items[i].sellIn < 6) {
                             if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
+                                this.items[i].increaseQuality()
                             }
                         }
                     }
@@ -63,22 +69,22 @@ export class GildedRose {
                         if (this.items[i].quality > 0) {
                             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                                 // si SellIn est négatif je rediminue quality de 1
-                                this.items[i].quality = this.items[i].quality - 1
+                                this.items[i].decreaseQuality()
                             }
                             if (this.items[i].name == 'Conjured') {
                                 if (this.items[i].quality > 0) {
-                                    this.items[i].quality = this.items[i].quality - 1
+                                    this.items[i].decreaseQuality()
                                 }
                             }
                         }
                     } else {
                         // je met backstage à 0 si sellIn = 0
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
+                        this.items[i].quality = 0
                     }
                 } else {
                     // j'augmente aged brie de 1 quality est pas de 50
                     if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
+                        this.items[i].increaseQuality()
                     }
                 }
             }
