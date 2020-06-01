445fa0dff6c0a1d01ff7c65db7eae23f3b8bef24
Phil <p.vancaloen@hotmail.com>
2020-06-01 09:33:22 +0200

    adding comments

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -8,10 +8,10 @@ export class Item {
         this.sellIn = sellIn;
         this.quality = quality;
     }
-    public increaseQuality() {
+    increaseQuality() {
         return this.quality = this.quality + 1
     }
-    public decreaseQuality() {
+    decreaseQuality() {
         return this.quality = this.quality - 1
     }
 }
@@ -27,22 +27,24 @@ export class GildedRose {
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                // diminue la qualité de -1 
+                // diminue la qualité de 1 
                 if (this.items[i].quality > 0) {
                     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                         this.items[i].decreaseQuality()
                     }
                 }
+
+                // si c'est un item conjured je diminue quality une deuxième fois
                 if (this.items[i].name == 'Conjured') {
                     if (this.items[i].quality > 0) {
                         this.items[i].decreaseQuality()
                     }
                 }
             } else {
-                // j'augemente la quality de 1
+                // j'augemente la quality de 1 
                 if (this.items[i].quality < 50) {
                     this.items[i].increaseQuality()
-                    // je continue à augmenté pour backstage
+                    // je continue à augmenter pour backstage
                     if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
                         // augemente backstage quality
                         if (this.items[i].sellIn < 11) {
@@ -62,7 +64,7 @@ export class GildedRose {
                 // j'update sellIn de -1
                 this.items[i].sellIn = this.items[i].sellIn - 1;
             }
-            // si sellIn passe en négatif je continue à diminue
+            // si sellIn passe en négatif je continue à diminuer qualité
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -71,6 +73,8 @@ export class GildedRose {
                                 // si SellIn est négatif je rediminue quality de 1
                                 this.items[i].decreaseQuality()
                             }
+
+                            // je continue de diminuer quality de 1 pour Conjured item
                             if (this.items[i].name == 'Conjured') {
                                 if (this.items[i].quality > 0) {
                                     this.items[i].decreaseQuality()
@@ -82,7 +86,7 @@ export class GildedRose {
                         this.items[i].quality = 0
                     }
                 } else {
-                    // j'augmente aged brie de 1 quality est pas de 50
+                    // j'augmente aged brie de 1 quality une deuxième fois
                     if (this.items[i].quality < 50) {
                         this.items[i].increaseQuality()
                     }
